39c84d737d328a8ac97ff0a8ee92e6bb716c2b00
dfealves <daniloferreira.alves@outlook.com>
2020-05-12 16:19:48 -0300

    added expected toMatchObject

--- __tests__/integration/products.test.js
+++ __tests__/integration/products.test.js
@@ -17,11 +17,8 @@ describe("handle products api", () => {
   it("should create a product", async () => {
     const product = await Product.create({
       title: "Produto 01",
-      slug: "algum slug",
       description: "descrição legal",
       price: 199,
-      active: true,
-      tag: ["tag 01"],
     });
 
     const response = await request(app).post("/products").send({
@@ -30,11 +27,15 @@ describe("handle products api", () => {
     });
 
     expect(response.status).toBe(201);
-    expect(product.title).toBe("Produto 01");
+    expect(response.body).toMatchObject({
+      title: "Produto 01",
+      description: "descrição legal",
+      price: 199,
+    });
   });
 
   it("should list all products", async () => {
-    const response = await request(app).get("/productss");
+    const response = await request(app).get("/products");
 
     expect(response.status).toBe(200);
     expect(response.body.length).toBeGreaterThan(0);
